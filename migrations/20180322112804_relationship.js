5307202eeb311dae7ddaefd4a6b14d5bd5bce5c1
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-03-22 12:15:29 -0600

    fixed small issue

--- migrations/20180322112804_relationship.js
+++ migrations/20180322112804_relationship.js
@@ -3,9 +3,9 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("relationship", (table) => {
     table.increments();
     table.integer("user1_id").notNullable().unsigned();
-    table.foreign("user1_id").references("users.id");
+    table.foreign("user1_id").references("customer.id");
     table.integer("user2_id").notNullable().unsigned();
-    table.foreign("user2_id").references("users.id");
+    table.foreign("user2_id").references("customer.id");
   });
 };
 
