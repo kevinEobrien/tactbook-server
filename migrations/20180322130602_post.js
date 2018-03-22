cc7f81c7168bb6db3388c7474695a723f280a4c4
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-03-22 13:44:26 -0600

    fixed error

--- migrations/20180322130602_post.js
+++ migrations/20180322130602_post.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("post", (table) => {
     table.increments().notNullable();
-    table.integer("customer_id").notNullabl();
+    table.integer("customer_id").notNullable();
     table.foreign("customer_id").references("customer.id");
     table.text("content");
     table.text("imageUrl1");
