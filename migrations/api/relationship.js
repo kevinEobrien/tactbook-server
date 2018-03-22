9c08d71b2a17bf567fe1e64f6d5fb506852e2b5f
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-03-22 12:31:13 -0600

    changed other route

--- migrations/api/relationship.js
+++ migrations/api/relationship.js
@@ -2,7 +2,7 @@ const express= require("express");
 
 const router= express.Router();
 
-//all routes will be prepended by /api/v1/users
+//all routes will be prepended by /api/v1/relationship
 
 router.get("/", (request, response) => {
   response.json([]);
