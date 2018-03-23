84a1ea959341468fdfea26016f72bd8f2146ee7e
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-03-23 11:23:15 -0600

    going to recreate database

--- migrations/20180322112804_relationship.js
+++ migrations/20180322112804_relationship.js
@@ -2,10 +2,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("relationship", (table) => {
     table.increments();
-    table.integer("user1_id").notNullable().unsigned();
-    table.foreign("user1_id").references("customer.id");
-    table.integer("user2_id").notNullable().unsigned();
-    table.foreign("user2_id").references("customer.id");
+    table.integer("customer_id").notNullable().unsigned();
+    table.foreign("customer_id").references("customer.id");
+    table.integer("customer2_id").notNullable().unsigned();
+    table.foreign("customer2_id").references("customer.id");
   });
 };
 
