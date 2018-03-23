85dff36c49782df84f12917a13217e2595968770
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-03-23 11:46:36 -0600

    :sparkles: all migrations and seeds done. new ERD added to README

--- seeds/01-post.js
+++ seeds/01-post.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex("post").del()
+  return knex.raw("TRUNCATE post CASCADE")
     .then(function () {
       // Inserts seed entries
       return knex("post").insert([
