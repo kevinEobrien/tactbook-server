d8aa704483e6f716057828047ee72d6207e4b096
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-03-24 19:52:43 -0600

    :sparkles: watson function yields correct categerories

---
+++ queries.js
@@ -0,0 +1,70 @@
+const database = require("./database-connection");
+const watson = require("./watson");
+
+
+module.exports = {
+  listCustomers(){
+    return database("customer");
+  },
+  readCustomer(id){
+    return database("customer").where("id", id).first();
+  },
+  createCustomer(customer){
+    return database("customer").insert(customer).returning("*").then(record => record[0]);
+  },
+  deleteCustomer(id){
+    return database("customer").delete().where("id", id);
+  },
+  updateCustomer(id, customer){
+    return database("customer").update(customer).where("id", id).returning("*").then(record => record[0]);
+  },
+  listRelationship(){
+    return database("relationship");
+  },
+  readRelationship(id){
+    return database("relationship").where("id", id).first();
+  },
+  //   createRelationship(customer_id, customer2_id){
+  //     return database("realtionship").insert(customer, customer_2).returning("*").then(record => record[0]);
+  //   },
+  deleteRelationship(id){
+    return database("relationship").delete().where("id", id);
+  },
+  listPost(){
+    return database("post");
+  },
+  readPost(id){
+    return database("post").where("id", id).first();
+  },
+  createPost(post){
+    watson(post.content)
+      .then(results => {
+        let category1 = results.categories[0].label.split("/");
+        let category2 = results.categories[1].label.split("/");
+        let category3 = results.categories[2].label.split("/");
+        let categories = category1.concat(category2, category3);
+        console.log(categories);
+      });
+    //   .then(categories => {
+    //      if (categories.includes("politics") || categories.includes("government")) {
+    //   return "I'm sorry, Watson says you are posting content that violates the terms of Tactbook's posting";
+    //   })
+    // console.log("filter.categories yields "+ filter.categories);
+    // let category1 = filter.categories[0].label.split("/");
+    // let category2 = filter.categories[1].label.split("/");
+    // let category3 = filter.categories[2].label.split("/");
+    // let categories = category1.concat(category2, category3);
+    // console.log(categories);
+    // if (categories.includes("politics") || categories.includes("government")) {
+    //   return "I'm sorry, Watson says you are posting content that violates the terms of Tactbook's posting";
+       
+    return database("post").insert(post).returning("*").then(record => record[0]);
+    
+  },
+  deletePost(id){
+    return database("post").delete().where("id", id);
+  },
+  updatepost(id, post){
+    return database("post").update(post).where("id", id).returning("*").then(record => record[0]);
+  }
+};
