40e96a8cffd2e6226486a2bb4eeddee87922600d
KEVIN OBRIEN <kevin.eugene.obrien@gmail.com>
2018-03-27 11:47:28 -0600

    :wrench: watson stops political posts

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
+require ("dotenv").load();
 const express = require("express");
 const cors = require("cors");
-const path= require("path");
+const database= require("./database-connection");
 const bodyParser= require("body-parser");
 const queries = require("./queries");
 const app= express();
 const watson= require("./watson");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const multer = require("multer");
 
-// const relationship= require("./migrations/api/relationship");
 
 app.use(cors());
 app.use(bodyParser.json());
+
+const s3 = new aws.S3({
+  apiVersion: "2006-03-01",
+  region: "us-east-1",
+  credentials: {
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID
+  }
+});
+    
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: "tactbook",
+    key: (request, file, next) => {
+      next(null, `${Date.now()}_${file.originalname}`);
+    }
+  })
+});
+
+app.get("/upload", (request, response, next) => {
+  response.json({
+    message: "Testing out the upload route"
+  });
+});
+
+app.post("/upload", upload.array("image", 1), (request, response) => {
+  response.json({
+    imgUrl: `${request.files[0].location}`
+  });
+});
 
 app.get("/", (request, response, next) => {
   response.json({
@@ -67,18 +101,18 @@ app.get("/post/:id", (request, response, next) => {
   
     
 app.post("/post", (request, response, next) => {
-  queries.createPost(request.body)
-    .then((result) => {
-      console.log("result is",result);
-      getCategories(result)
-        .then(categories => {
-          console.log(categories);
-          if(categories.includes("politics") || categories.includes("government")){
-            response.status(201).json("I'm sorry, Watson says you are posting content that violates the terms of Tactbook's posting");
-          }else{
+  getCategories(request.body)
+    .then(categories => {
+      console.log(categories);
+      if(categories.includes("politics") || categories.includes("government")){
+        response.status(201).json("I'm sorry, Watson says you are posting content that violates the terms of Tactbook's posting policy.");
+      }else{
+        return queries.createPost(request.body)
+          .then((result) => {
             response.status(201).json("Post successful");
-          }
-        });
+            console.log("result is",result);
+          });
+      }
     }).catch(next);
 });
 
@@ -92,6 +126,15 @@ app.put("/post/:id", (request, response, next) => {
   queries.updatePost(request.params.id, request.body).then(() => {
     response.status(201).json("Post updated.");
   }).catch(next);
+});
+
+app.put ("/post/:id", (request, response, next) => {
+  database("post").select("likes").where("id", request.params.id)
+    .then((currentLikes) => queries.updateLikes(request.params.id, currentLikes[0].likes))
+    .then(comment => {
+      response.send(comment);
+    })
+    .catch(next);
 });
 
 app.get("/comment", (request, response, next) => {
